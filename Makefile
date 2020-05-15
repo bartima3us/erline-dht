a03ae9791a71e3c74c34a12cae92e377790ebeda
Šarūnas <sarunas0919@gmail.com>
2020-05-15 19:23:25 +0300

    Added third node to ping CT.

--- Makefile
+++ Makefile
@@ -2,5 +2,4 @@ start:
 	rebar3 shell --config=config/sys.config --sname erlinedht
 
 tests:
-	rebar3 eunit
 	rebar3 ct --verbose --sys_config=test/sys.config --sname erlinedht-test
--- test/erline_dht_bucket_SUITE.erl
+++ test/erline_dht_bucket_SUITE.erl
@@ -62,30 +62,39 @@ test_ping(_Config) ->
     % Start nodes
     {ok, PidNode1} = erline_dht_bucket:start_link(node1, 0),
     {ok, PidNode2} = erline_dht_bucket:start_link(node2, 0),
+    {ok, PidNode3} = erline_dht_bucket:start_link(node3, 0),
     % Get port
     PortNode2 = erline_dht_bucket:get_port(PidNode2),
+    PortNode3 = erline_dht_bucket:get_port(PidNode3),
     % Get hash
     HashNode2 = erline_dht_bucket:get_hash(PidNode2),
-    % Event manager
-    EventMgrPid = erline_dht:get_event_mgr_pid(node1),
-    gen_event:add_handler(EventMgrPid, erline_dht_event_handler, []),
+    HashNode3 = erline_dht_bucket:get_hash(PidNode3),
+    % Node1 event manager
+    EventMgrPid1 = erline_dht:get_event_mgr_pid(node1),
+    gen_event:add_handler(EventMgrPid1, erline_dht_event_handler, []),
     ok = erline_dht_bucket:add_node(node1, {127,0,0,1}, PortNode2),
     ok = meck:wait(erline_dht_event_handler, handle_event, [{ping, q, {127,0,0,1}, PortNode2, HashNode2}, '_'], 10000),
     1 = meck:num_calls(erline_dht_event_handler, handle_event, [{ping, q, {127,0,0,1}, PortNode2, HashNode2}, '_']),
+    % Node2 event manager
+    EventMgrPid2 = erline_dht:get_event_mgr_pid(node2),
+    gen_event:add_handler(EventMgrPid2, erline_dht_event_handler, []),
+    ok = erline_dht_bucket:add_node(node2, {127,0,0,1}, PortNode3),
+    ok = meck:wait(erline_dht_event_handler, handle_event, [{ping, q, {127,0,0,1}, PortNode3, HashNode3}, '_'], 10000),
+    1 = meck:num_calls(erline_dht_event_handler, handle_event, [{ping, q, {127,0,0,1}, PortNode3, HashNode3}, '_']),
+    %
     true = meck:validate(erline_dht_event_handler),
     ok = meck:unload(erline_dht_event_handler),
     % Check whether each node has the other node in it's bucket
-    [#{distance := Distance, nodes := 1}] = lists:filter(fun
-        (#{nodes := 0}) -> false;
-        (#{nodes := 1}) -> true
-    end, erline_dht_bucket:get_buckets_filling(node1)),
-    [#{distance := Distance, nodes := 1}] = lists:filter(fun
-        (#{nodes := 0}) -> false;
-        (#{nodes := 1}) -> true
-    end, erline_dht_bucket:get_buckets_filling(node2)),
+    2 = lists:foldl(fun(#{nodes := Nodes}, AccTotal) ->
+        AccTotal + Nodes
+    end, 0, erline_dht_bucket:get_buckets_filling(node1)),
+    2 = lists:foldl(fun(#{nodes := Nodes}, AccTotal) ->
+        AccTotal + Nodes
+    end, 0, erline_dht_bucket:get_buckets_filling(node2)),
     % Stop the nodes
     ok = erline_dht_bucket:stop(PidNode1),
     ok = erline_dht_bucket:stop(PidNode2),
+    ok = erline_dht_bucket:stop(PidNode3),
     ok.
 
 
