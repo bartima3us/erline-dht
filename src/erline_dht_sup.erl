54e3bd30817d8fb7472c3591a182c026bac307f8
Šarūnas <sarunas0919@gmail.com>
2020-09-02 21:42:42 +0300

    Supervisor children spec fix.

--- src/erline_dht_sup.erl
+++ src/erline_dht_sup.erl
@@ -21,23 +21,24 @@
 -define(SERVER, ?MODULE).
 -define(BUCKET_ID(Name), {erline_dht_bucket, Name}).
 -define(NAN_CACHE_ID(Name), {erline_dht_nan_cache, Name}).
--define(SPEC(Name, Port), [
-#{
-        id          => ?BUCKET_ID(Name),
-        start       => {erline_dht_bucket, start_link, [Name, Port]},
-        restart     => temporary,
-        shutdown    => 5000,
-        type        => worker,
-        modules     => [erline_dht_bucket]
-    },
-    #{
-        id          => ?NAN_CACHE_ID(Name),
-        start       => {erline_dht_nan_cache, start_link, [Name]},
-        restart     => temporary,
-        shutdown    => 5000,
-        type        => worker,
-        modules     => [erline_dht_nan_cache]
-    }]).
+
+-define(BUCKET_SPEC(Name, Port), #{
+    id          => ?BUCKET_ID(Name),
+    start       => {erline_dht_bucket, start_link, [Name, Port]},
+    restart     => temporary,
+    shutdown    => 5000,
+    type        => worker,
+    modules     => [erline_dht_bucket]
+}).
+
+-define(CACHE_SPEC(Name), #{
+    id          => ?NAN_CACHE_ID(Name),
+    start       => {erline_dht_nan_cache, start_link, [Name]},
+    restart     => temporary,
+    shutdown    => 5000,
+    type        => worker,
+    modules     => [erline_dht_nan_cache]
+}).
 
 %%====================================================================
 %% API functions
@@ -60,7 +61,8 @@ start(Name, Port) ->
     Children = supervisor:which_children(?MODULE),
     case lists:keysearch(?BUCKET_ID(Name), 1, Children) of
         false      ->
-            {ok, _} = supervisor:start_child(?MODULE, ?SPEC(Name, Port)),
+            {ok, _} = supervisor:start_child(?MODULE, ?BUCKET_SPEC(node1, Port)),
+            {ok, _} = supervisor:start_child(?MODULE, ?CACHE_SPEC(node1)),
             ok;
         {value, _} ->
             ok
@@ -72,8 +74,8 @@ start(Name, Port) ->
 %%
 init([]) ->
     Specs = case erline_dht:get_env(node1, auto_start) of
-        true      -> ?SPEC(node1, undefined);
-        undefined -> ?SPEC(node1, undefined);
+        true      -> [?BUCKET_SPEC(node1, undefined), ?CACHE_SPEC(node1)];
+        undefined -> [?BUCKET_SPEC(node1, undefined), ?CACHE_SPEC(node1)];
         false     -> []
     end,
     {ok, {{one_for_all, 5, 10}, Specs}}.
